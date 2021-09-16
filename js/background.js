6deebc6c965cc16af77043c7cdfc1b30868bd65a
xu-ux <52966229+xu-ux@users.noreply.github.com>
2021-09-16 15:11:19 +0800

    feat: add copy url
    增强复制url

--- js/background.js
+++ js/background.js
@@ -14,6 +14,15 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
 // 右键菜单
 chrome.contextMenus.create({
-	title: "获取当前页面信息",
-	onclick:(info,tab) => { alert(`当前地址：${info.pageUrl} 窗口索引：${tab.index}`)}
+    title: "获取当前页面信息",
+    onclick:(info,tab) => {
+        let pageUrl = info.pageUrl;
+        let inputDom =document.createElement('input');
+        inputDom.style.opacity = 0;//设置input标签设置为透明(不可见)
+        inputDom.value = pageUrl; // 修改文本框的内容
+        document.body.appendChild(inputDom);
+        inputDom.select(); // 选中文本
+        document.execCommand("copy");
+        alert(`当前地址：${pageUrl} 已复制到粘贴板`)
+    }
 })
